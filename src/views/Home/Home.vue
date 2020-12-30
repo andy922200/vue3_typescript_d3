7d2b6ba93c60047c217a5bb8e21e810b3329e9e0
andy lien <andy922200@gmail.com>
2020-12-30 19:45:49 +0800

    modified default data

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -24,7 +24,7 @@ import {
 export default defineComponent({
     name: 'Home',
     setup () {
-        const dataSet = reactive([70, 130, 120, 95, 170, 143])
+        const dataSet = reactive([70, 130, 120, 95, 170, 143, 180, 250, 300])
         const barChartSettings = {
             width: 400,
             height: 400,
@@ -45,29 +45,12 @@ export default defineComponent({
                 textAnchor: 'middle',
                 dy: '1em'
             },
-            maxValue: 300
+            maxValue: 400
         }
-        const barChartSettingsRWD = {
-            padding: {
-                top: 20,
-                right: 20,
-                bottom: 20,
-                left: 20
-            },
-            rect: {
-                color: 'steelblue'
-            },
-            text: {
-                color: 'white',
-                fontSize: '14px',
-                textAnchor: 'middle',
-                dy: '1em'
-            },
-            maxValue: 300
-        }
+
         onMounted(() => {
             const barChartDiv = document.querySelector('.barChart')
-            const testBarChart = new BarChart(barChartSettingsRWD)
+            const testBarChart = new BarChart(barChartSettings)
             testBarChart.rwdDraw(dataSet, '.barChart', barChartDiv)
         })
     }
